6e14b117572d1493baae330292bfa29acd239b32
nikhilgupta321 <nikhilgupta321@gmail.com>
2023-09-13 05:26:20 +0000

    subject

--- client/core/Frame.js
+++ client/core/Frame.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Frame(props) {
   return (
     <div>
-      <div className="title frame-title text-xl text-gray-500">{props.title} <hr className=" border-gray-500" /></div>
+      <div className="text-xl text-gray-500 bg-gray-200 title frame-title">{props.title} <hr className="border-gray-500 " /></div>
       <div className="">{props.children}</div>
     </div>
   );
