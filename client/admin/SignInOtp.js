443830e9293726aa01faa102cb6928676298650b
nikhilgupta321 <nikhilgupta321@gmail.com>
2024-08-08 11:11:43 +0000

    controller change

--- client/admin/SignInOtp.js
+++ client/admin/SignInOtp.js
@@ -50,42 +50,44 @@ export default function SignIn(props) {
   }
 
   const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value , error: ''})
+    setValues({ ...values, [name]: event.target.value, error: '' })
   }
 
-  return (<>
-    <div className='min-h-screen bg-gray-100'>
-      {values.verified && (
-        <Navigate to="/admin" replace={true} />
-      )}
-      <div className="flex items-center justify-center pt-40">
-        <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
-          <h3 className="text-2xl font-bold text-center">Login to your account</h3>
-          <p className='h-4 text-red-500'>{values.error}</p>
-          {!values.otpSent && <div className="mt-4">
-            <div>
-              <input type="text" placeholder="Username" value={values.username} onChange={handleChange('username')}
-                className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+  return (
+    <>
+      <div className='min-h-screen bg-gray-100'>
+        {values.verified && (
+          <Navigate to="/admin" replace={true} />
+        )}
+        <div className="flex items-center justify-center pt-40">
+          <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
+            <h3 className="text-2xl font-bold text-center">Login to your account</h3>
+            <p className='h-4 text-red-500'>{values.error}</p>
+            {!values.otpSent && <div className="mt-4">
+              <div>
+                <input type="text" placeholder="Username" value={values.username} onChange={handleChange('username')}
+                  className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+              </div>
+              <div className="flex items-center justify-center">
+                <button type="button" onClick={clickSendOtp}
+                  className="px-6 py-2 mt-8 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Send OTP</button>
+              </div>
             </div>
-            <div className="flex items-center justify-center">
-              <button type="button" onClick={clickSendOtp}
-                className="px-6 py-2 mt-8 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Send OTP</button>
+            }
+            {values.otpSent && <div className="mt-4">
+              <div>
+                <input type="text" placeholder="OTP" value={values.otpCode} onChange={handleChange('otpCode')}
+                  className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+              </div>
+              <div className="flex items-center justify-center">
+                <button type="button" onClick={clickVerifyOtp}
+                  className="px-6 py-2 mt-8 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Sign In</button>
+              </div>
             </div>
+            }
           </div>
-          }
-          {values.otpSent && <div className="mt-4">
-            <div>
-              <input type="text" placeholder="OTP" value={values.otpCode} onChange={handleChange('otpCode')}
-                className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
-            </div>
-            <div className="flex items-center justify-center">
-              <button type="button" onClick={clickVerifyOtp}
-                className="px-6 py-2 mt-8 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Sign In</button>
-            </div>
-          </div>
-          }
         </div>
       </div>
-    </div>
-  </>)
+    </>
+  )
 }
