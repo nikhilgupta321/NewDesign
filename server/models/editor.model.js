57e677ba7ebf17b3bb89840b3e406ceeb5aceed1
nikhilgupta321 <nikhilgupta321@gmail.com>
2024-09-07 08:04:51 +0000

    add postion in editor modle

--- server/models/editor.model.js
+++ server/models/editor.model.js
@@ -1,90 +1,96 @@
 import { sequelize } from "../../config/config";
 import { DataTypes } from "sequelize";
 
-const Editor = sequelize.define('editors', {
-  id: {
-    autoIncrement: true,
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true
-  },
-  category: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  subcategory: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  name: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  degree: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  post: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    defaultValue: '',
-  },
-  email: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  phone: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  country: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  picture: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    defaultValue: '',
-  },
-  status: {
-    type: DataTypes.ENUM('enabled','disabled'),
-    allowNull: false,
-    defaultValue: 'enabled',
-  },
-  creation: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: true
-  }
-}, {
-  sequelize,
-  tableName: 'editors',
-  timestamps: false,
-  indexes: [
-    {
-      name: "PRIMARY",
-      unique: true,
-      using: "BTREE",
-      fields: [
-        { name: "id" },
-      ]
+const Editor = sequelize.define(
+  "editors",
+  {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
     },
-  ]
-});
+    postion: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    category: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    subcategory: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    degree: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    post: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: "",
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    phone: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    country: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    picture: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: "",
+    },
+    status: {
+      type: DataTypes.ENUM("enabled", "disabled"),
+      allowNull: false,
+      defaultValue: "enabled",
+    },
+    creation: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: "editors",
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [{ name: "id" }],
+      },
+    ],
+  }
+);
 
 export default Editor;
